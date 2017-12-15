e32c90424b35baa946b077b88152105103c6bfe8
lambert2016 <lambert2017@scryptmail.com>
2017-12-15 20:46:13 +0800

    before reffactor

--- lists/tests.py
+++ lists/tests.py
@@ -7,8 +7,14 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
 
-	def test_home_page_returns_correct_html(self):
+	def test_home_page_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
 
-		
+	def test_can_save_a_POST_request(self):
+		text = 'A new list item'
+		response = self.client.post('/', data={'item_text': text})
+		self.assertIn(text, response.content.decode())
+		self.assertTemplateUsed(response, 'home.html')
+
+	
