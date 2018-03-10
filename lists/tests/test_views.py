3596f16e771c3862ef2e7c5547797cca7857ee5f
konni <lambert2017@scryptmail.com>
2018-03-10 10:58:08 +0800

    Split out unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,44 +5,12 @@ from django.http import HttpRequest
 from lists.views import home_page
 from lists.models import Item, List
 
-# Create your tests here.
+
 class HomePageTest(TestCase):
 
 	def test_home_page_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
-
-
-class ListAndItemModelTest(TestCase):
-
-	def test_saving_and_retrieving_items(self):
-		list_ = List()
-		list_.save()
-
-		first_item = Item()
-		first_text = 'The first list item'
-		first_item.text = first_text
-		first_item.list = list_
-		first_item.save()
-
-		second_item = Item()
-		second_text = 'Item the second'
-		second_item.text = second_text
-		second_item.list = list_
-		second_item.save()
-
-		saved_list = List.objects.first()
-		self.assertEqual(saved_list, list_)
-
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
-
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual(first_saved_item.text, first_text)
-		self.assertEqual(first_saved_item.list, list_)
-		self.assertEqual(second_saved_item.text, second_text)
-		self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
