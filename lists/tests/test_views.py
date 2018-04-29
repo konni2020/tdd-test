5f6b4436b6bd0d6a09e23dc1659f3a5415b837a9
konni <test@test.com>
2018-04-29 14:19:00 +0800

    quick commit

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,3 +1,5 @@
+from unittest import skip
+
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
@@ -88,6 +90,20 @@ class ListViewTest(TestCase):
 	def test_for_invalid_input_shows_error_on_page(self):
 		response = self.post_invalid_input()
 		self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+	
+	@skip
+	def test_for_duplicate_item_validation_errors_on_page(self):
+		list1 = List.objects.create()
+		item1 = Item.objects.create(list=list1, text='textey')
+		response = self.client.post(
+			'/lists/{}/'.format(list1.id),
+			data={'text': 'textey'}
+		)
+
+		expected_error = escape("You've already got this in your list")
+		self.assertContains(response, expected_error)
+		self.assertTemplateUsed(response, 'list.html')
+		self.assertEqual(Item.objects.count(), 1)
 
 	def test_displays_item_form(self):
 		list_ = List.objects.create()
