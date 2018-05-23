b7c973d87b8baced2e32b49416175c23b1fb1381
konni <test@test.com>
2018-05-23 22:20:30 +0800

    test_my_lists: precreate sessions, move login checks into base

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -48,19 +48,11 @@ class LoginTest(FunctionalTest):
 		self.assertIn(TEST_EMAIL, navbar.text)
 
 		# she is logged in!
-		self.wait_for(
-			lambda: self.browser.find_element_by_link_text('Log out')
-		)
-		navbar = self.browser.find_element_by_css_selector('.navbar')
-		self.assertIn(TEST_EMAIL, navbar.text)
+		self.wait_to_be_logged_in(TEST_EMAIL)
 
 		# now she logs out
 		self.browser.find_element_by_link_text('Log out').click()
 
 		# she is logged out
-		self.wait_for(
-			lambda: self.browser.find_element_by_name('email')
-		)		
-		navbar = self.browser.find_element_by_css_selector('.navbar')
-		self.assertNotIn(TEST_EMAIL, navbar.text)
+		self.wait_to_be_logged_out(TEST_EMAIL)
 
