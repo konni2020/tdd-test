f9ecdf64c39634e43d8f20e1861c787dd8f4b902
konni <test@test.com>
2018-05-23 10:27:34 +0800

    chapter 19

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -46,3 +46,21 @@ class LoginTest(FunctionalTest):
 		)
 		navbar = self.browser.find_element_by_css_selector('.navbar')
 		self.assertIn(TEST_EMAIL, navbar.text)
+
+		# she is logged in!
+		self.wait_for(
+			lambda: self.browser.find_element_by_link_text('Log out')
+		)
+		navbar = self.browser.find_element_by_css_selector('.navbar')
+		self.assertIn(TEST_EMAIL, navbar.text)
+
+		# now she logs out
+		self.browser.find_element_by_link_text('Log out').click()
+
+		# she is logged out
+		self.wait_for(
+			lambda: self.browser.find_element_by_name('email')
+		)		
+		navbar = self.browser.find_element_by_css_selector('.navbar')
+		self.assertNotIn(TEST_EMAIL, navbar.text)
+
